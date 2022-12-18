d69f0a0f759422db8e11681eff201d56cd2f0d46
Irem Kenar <ikenar@kazandirio-dev.com>
2022-12-18 15:07:42 +0300

    initial commit

---
+++ with-typescript.js
@@ -0,0 +1,5 @@
+function addAgain(a, b) {
+    return a + b;
+}
+var newResult = addAgain(2, 5);
+console.log(newResult);
---
+++ with-typescript.ts
@@ -0,0 +1,7 @@
+function addAgain(a: number, b: number) {
+  return a + b;
+}
+
+const newResult = addAgain(2, 5);
+
+console.log(newResult);
